ff4c7868ddcfe29647595694d829ffa8131521d0
Angel Laika Clarito <angellaikaclarito04@gmail.com>
2025-02-25 21:00:56 +0800

    Update TheFirstGame.py
    Fixing key (Gas and break not working)

--- TheFirstGame.py
+++ TheFirstGame.py
@@ -1,10 +1,18 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+from pynput.keyboard import Controller, Key
+import time
 
-detector = HandDetector(detectionCon=0.5, maxHands=2)
+keyboard = Controller()
+
+detector = HandDetector(detectionCon=0.7, maxHands=2)
+
 cap = cv2.VideoCapture(0)
 cap.set (3, 600)
 cap.set (4,400)
+
+last_key = None
+
 while True:
     sucess, img = cap.read()
     img = cv2.flip (img, 1)
