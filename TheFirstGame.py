c012be035e5e211f59d28f30be158b2f0bbd35b1
Angel Laika Clarito <angellaikaclarito04@gmail.com>
2025-02-25 21:05:03 +0800

    Update TheFirstGame.py
    Fix the key completely. (Break and Gas are now working)

--- TheFirstGame.py
+++ TheFirstGame.py
@@ -14,12 +14,33 @@ cap.set (4,400)
 last_key = None
 
 while True:
-    sucess, img = cap.read()
-    img = cv2.flip (img, 1)
-    hand, img = detector.findHands(img)
-    if hand and hand[0]["type"] == "Left":
-        fingers = detector.fingersUp(hand[0])
-        totalFingers = fingers.count(1)
-        cv2.putText (img, f'Fingers: {totalFingers}', (50, 50), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 0), 2)
-    cv2.imshow ('Camera Feed', img)
-    cv2.waitKey(1)
+    success, img = cap.read()
+    img = cv2.flip(img, 1)  
+    hands, img = detector.findHands(img)
+
+    if hands:
+        hand = hands[0]  
+        if hand["type"] == "Left":  
+            fingers = detector.fingersUp(hand)
+            totalFingers = fingers.count(1)
+
+            cv2.putText(img, f'Fingers: {totalFingers}', (50, 50), 
+                        cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 0), 2)
+
+            
+            if totalFingers == 5 and last_key != "right":
+                keyboard.press(Key.right)
+                keyboard.release(Key.left)
+                last_key = "right"
+            elif totalFingers == 0 and last_key != "left":
+                keyboard.press(Key.left)
+                keyboard.release(Key.right)
+                last_key = "left"
+
+    cv2.imshow('Camera Feed', img)
+
+    if cv2.waitKey(1) & 0xFF == 1:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
